beff8dafcecc53a147dcc670db9ed310969c8ef1
Boden Fuller <boden.fuller@gmail.com>
2026-02-20 07:15:12 -0500

    fix(cli): restore hooks install symbols used by init and doctor

--- cli/cmd/ao/hooks.go
+++ cli/cmd/ao/hooks.go
@@ -19,6 +19,7 @@ var (
 	hooksDryRun       bool
 	hooksForce        bool
 	hooksFull         bool
+	hooksMinimal      bool
 	hooksSourceDir    string
 )
 
@@ -1079,4 +1080,14 @@ func runHooksTest(cmd *cobra.Command, args []string) error {
 	}
 
 	return nil
+}
+
+func countInstalledEvents(hooksMap map[string]interface{}) int {
+	installed := 0
+	for _, event := range AllEventNames() {
+		if groups, ok := hooksMap[event].([]interface{}); ok && len(groups) > 0 {
+			installed++
+		}
+	}
+	return installed
 }
