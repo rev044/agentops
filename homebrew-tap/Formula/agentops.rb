5128cbeb9be125a66c2a9941b16c085c03710e04
Boden Fuller <boden.fuller@gmail.com>
2026-03-31 10:51:31 -0400

    chore(brew): update formula to v2.31.0 with pre-built binaries

--- homebrew-tap/Formula/agentops.rb
+++ homebrew-tap/Formula/agentops.rb
@@ -4,23 +4,31 @@
 class Agentops < Formula
   desc "AI-assisted development workflow CLI"
   homepage "https://github.com/boshu2/agentops"
-  version "1.0.0"
+  version "2.31.0"
   license "MIT"
 
-  # TODO: Update URL when ao CLI is released
-  # url "https://github.com/boshu2/agentops/releases/download/v#{version}/ao-#{version}-darwin-amd64.tar.gz"
-  # sha256 "PLACEHOLDER"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/boshu2/agentops/releases/download/v#{version}/ao-darwin-arm64.tar.gz"
+      sha256 "aad4483f0b3209a02d10abf412eb29e78475312061d05d6a71c0fb4224bb5056"
+    else
+      url "https://github.com/boshu2/agentops/releases/download/v#{version}/ao-darwin-amd64.tar.gz"
+      sha256 "3ad4c80b1a5784c8bbbdb892feef8e10e695fae31f6f39ee0fa74c264086a091"
+    end
+  end
 
-  # For development, build from source
-  head "https://github.com/boshu2/agentops.git", branch: "main"
-
-  depends_on "go" => :build
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/boshu2/agentops/releases/download/v#{version}/ao-linux-arm64.tar.gz"
+      sha256 "37e674a6311004a1031cbb90886bc9aef75b70ced8e6d21b65ab8001788a1687"
+    else
+      url "https://github.com/boshu2/agentops/releases/download/v#{version}/ao-linux-amd64.tar.gz"
+      sha256 "df8ff9a41d748552b7dfb67dc8e4e19628fdf0e51a04f315493305bab1554b7f"
+    end
+  end
 
   def install
-    # Build ao CLI from cli/ directory
-    cd "cli" do
-      system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/ao"
-    end
+    bin.install "ao"
   end
 
   def caveats
